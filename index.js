68823204e1505f5ad0f98c1de324ad67271a026e
robiulalam76 <robiulalam762400@gmail.com>
2022-10-25 16:39:39 +0600

    file change and code update

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = 5000
 app.use(cors())
 
 const courses = require('./data/courses.json')
-const course = require('./data/course.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,13 +14,10 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
-app.get('/course', (req, res) => {
-    res.send(course)
-})
 
 app.get('/course/:id', (req, res) => {
     const id = req.params.id
-    const selectedCourse = course.find(c => c.id === id)
+    const selectedCourse = courses.find(c => c.id === id)
     res.send(selectedCourse)
 })
 
